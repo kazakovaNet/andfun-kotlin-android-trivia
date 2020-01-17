6621021329a9a14f46176ae777951884c1723c15
NKazakova <NKazakova@IBS.RU>
2020-01-17 17:50:25 +0300

    Exercise 07-Adding-Safe-Arguments in process

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -37,6 +38,8 @@ class GameWonFragment : Fragment() {
                 Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_gameFragment)
         )
 
+        var args = GameWonFragmentArgs.fromBundle(this.arguments!!)
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
         return binding.root
     }
 }
